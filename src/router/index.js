89e25e5c880e48ddcf162fbe63d111428a25bfe7
manowski <manowski10@gmail.com>
2020-06-16 20:17:23 +0200

    Add routes

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,17 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/user/:name',
+    name: 'user',
+    component: () => import(/* webpackChunkName: "about" */ '../components/User.vue')
+  },
+  {
+    path: '/top/country/:name',
+    name: 'topbycountry',
+    props: true,
+    component: () => import(/* webpackChunkName: "about" */ '../components/TopByCountry.vue')
   }
 ]
 
