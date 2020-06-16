58ca5fb4e2942d540dab2bec0773e652d9e2c3a8
manowski <manowski10@gmail.com>
2020-06-16 20:13:24 +0200

    Initial

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+	  <Nav />
+    <router-view/>
+  </div>
+</template>
+<script>
+import Nav from "@/components/Nav.vue";
+
+export default {
+components: {
+    Nav,
+  }
+}
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+#app {
+  font-family: 'Roboto', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+}
+#nav {
+  background-image: url('../src/assets/img/back.jpg');
+  content: '';
+  top: 0;
+  width: 100%;
+  height: 100%;
+  padding: 0.5em 0 4em;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+<Section />
+
+ <div class="container-fluid">
+   <div class="row">
+     
+     <div class="col-lg-3 pt-5 sidebar">
+       <Sidebar />
+     </div>
+     <div class="col-lg-9 pt-5 main">
+       <Users />
+     </div>
+   </div>
+ </div>
+ </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Users from "@/components/Users.vue";
+import Sidebar from "@/components/Sidebar.vue";
+import Section from "@/components/Section.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    Users,
+    Sidebar,
+    Section,
+  }
+};
+</script>
+<style>
+#app {
+  background-color: #eff3f6;
+}
+.container-fluid {
+  width: 93% !important;
+}
+.container {
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 76rem;
+}
+.sidebar, .main {
+  background-color: #fff;;
+}
+</style>
