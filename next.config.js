5325973e171954c63d5ea92718a71e5e7579e9fc
manowski <manowski10@gmail.com>
2023-03-02 17:06:53 +0100

    Initial new project with nextjs

---
+++ next.config.js
@@ -0,0 +1,26 @@
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
+
+//module.exports = withBundleAnalyzer({})
+module.exports = {
+  images: {
+    domains: [
+      'images.ctfassets.net',
+      's3-us-west-2.amazonaws.com',
+      'res.cloudinary.com',
+      'res.cloudinary.com',
+      'cdn.insiflow.com',
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+         config.resolve.fallback.fs = false
+         config.resolve.fallback.dns = false
+         config.resolve.fallback.net = false
+         config.resolve.fallback.tls = false
+    }
+
+    return config;
+  }
+}
